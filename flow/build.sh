94ae97a48a184a843dec067a858b8da51f880580
Iván Perdomo <ivan@perdomo.me>
2017-03-14 12:24:25 +0100

    [#6] Update build process
    - Make sure the Docker images is present
    - Get dependencies
    - Build and install the jar

--- flow/build.sh
+++ flow/build.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
-docker run --rm --interactive --tty \
+set -eu
+
+./get-dependencies.sh
+
+IMAGE=$(docker images | grep 'akvo/flow-build' || echo -n "")
+
+if [[ -z "${IMAGE}" ]]; then
+    docker build -t akvo/flow-build .
+fi
+
+docker run --rm \
        --volume "$PWD/.akvo-flow-cache/akvo-flow":/opt/akvo-flow \
        --volume "$HOME/.m2/repository":/opt/.m2/repository \
        --user `id -u`:`id -g` \
-       akvo/flow-dev
+       akvo/flow-build
