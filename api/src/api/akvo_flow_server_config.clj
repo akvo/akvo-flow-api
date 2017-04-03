5d6ef8ff035211b1ac447636da20713927890419
Iván Perdomo <ivan@akvo.org>
2017-04-03 14:47:59 +0200

    [#8] Refactor code in a pairing session with @jonase

--- api/src/api/akvo_flow_server_config.clj
+++ api/src/api/akvo_flow_server_config.clj
@@ -21,31 +21,56 @@
                    {:headers (headers auth-token)
                     :as :json})))
 
-(defn get-instances [auth-token]
+(defn dir? [content-element]
+  (= "dir" (:type content-element)))
+
+(defn fetch-instance-ids [auth-token]
   (->> (github-contents auth-token "/")
-       (filter #(= "dir" (:type %)))
+       (filter dir?)
        (map :name)))
 
-(defn get-instance-props [instance-id auth-token]
-  (let [tmp-file (java.io.File/createTempFile "appengine" ".xml")
-        _ (spit tmp-file
-                (-> (github-contents auth-token (format "/%s/appengine-web.xml" instance-id))
+(defn fetch-file
+  "Fetch the file from github. Save it and return the File.
+  Returns nil if the file can't be found"
+  [auth-token instance-id file-path]
+  (try
+    (let [bytes (-> (github-contents auth-token file-path)
                     :content
-                    Base64/decodeBase64
-                    String.))
+                    Base64/decodeBase64)
+          file-name (.getName (io/file file-path))
+          file (io/file tmp-dir instance-id file-name)]
+      (.mkdirs (.getParentFile file))
+      (io/copy bytes file)
+      file)
+    (catch clojure.lang.ExceptionInfo e)))
+
+(defn get-file [auth-token instance-id file-path]
+  (let [file-name (.getName (io/file file-path))
+        file (io/file tmp-dir instance-id file-name)]
+    (if (.exists file)
+      file
+      (fetch-file auth-token instance-id file-path))))
+
+(defn get-p12-file [auth-token instance-id]
+  (get-file auth-token instance-id (format "/%1$s/%1$s.p12" instance-id)))
+
+(defn get-appengine-web-xml-file [auth-token instance-id]
+  (get-file auth-token instance-id (format "/%s/appengine-web.xml" instance-id)))
+
+(defn get-instance-props [auth-token instance-id]
+  (let [tmp-file (get-appengine-web-xml-file auth-token instance-id)
         ae-reader (AppEngineWebXmlReader. tmp-dir
                                           (.getName tmp-file))]
     (.getSystemProperties (.readAppEngineWebXml ae-reader))))
 
 (defn get-instance-map [auth-token]
-  (let [instances (get-instances auth-token)]
+  (let [instance-ids (fetch-instance-ids auth-token)]
     (reduce (fn [m instance-id]
               (try
-                (assoc m instance-id (get-instance-props instance-id auth-token))
+                (assoc m instance-id (get-instance-props auth-token instance-id))
                 (catch Exception e m)))
             {}
-            ["akvoflow-uat1"];; instances
-            )))
+            instance-ids)))
 
 (defn get-alias-map [instances-map]
   (reduce (fn [m [instance-id props]]
@@ -54,22 +79,3 @@
               (assoc m alias instance-id)))
           {}
           instances-map))
-
-(defn fetch-p12
-  "Fetch the p12 file from github. Save it and return the File.
-  Returns nil if the file can't be found"
-  [instance-id auth-token]
-  (try
-    (let [bytes (-> (github-contents auth-token (format "/%1$s/%1$s.p12" instance-id))
-                    :content
-                    Base64/decodeBase64)
-          file (io/file tmp-dir (str instance-id ".p12"))]
-      (io/copy bytes file)
-      file)
-    (catch clojure.lang.ExceptionInfo e)))
-
-(defn get-p12 [instance-id auth-token]
-  (let [file (io/file tmp-dir (str instance-id ".p12"))]
-    (if (.exists file)
-      file
-      (fetch-p12 instance-id auth-token))))
