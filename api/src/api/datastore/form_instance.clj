4d92e3c9b008d46ef3580a226fd8ee59eab74800
Jonas Enlund <jonas.enlund@gmail.com>
2017-04-03 11:16:58 +0300

    [#7] Resolve reflections for form-instances fetching

--- api/src/api/datastore/form_instance.clj
+++ api/src/api/datastore/form_instance.clj
@@ -5,7 +5,10 @@
             [cheshire.core :as json]
             [clojure.string :as s])
   (:refer-clojure :exclude [list])
-  (:import [com.fasterxml.jackson.core JsonParseException]))
+  (:import [com.fasterxml.jackson.core JsonParseException]
+           [com.google.appengine.api.datastore Entity Text QueryResultIterator QueryResultIterable]))
+
+(set! *warn-on-reflection* true)
 
 (def MAX_PAGE_SIZE 300)
 
@@ -16,14 +19,14 @@
       page-size
       MAX_PAGE_SIZE)))
 
-(defn form-instances-query [ds form-definition {:keys [cursor page-size]}]
+(defn form-instances-query ^QueryResultIterator [ds form-definition {:keys [cursor page-size]}]
   (let [page-size (normalize-page-size page-size)]
-    (.iterator (q/result ds
-                         {:kind "SurveyInstance"
-                          :filter (q/= "surveyId" (Long/parseLong (:id form-definition)))}
-                         {:start-cursor cursor
-                          :chunk-size page-size
-                          :limit page-size}))))
+    (.iterator ^QueryResultIterable (q/result ds
+                                              {:kind "SurveyInstance"
+                                               :filter (q/= "surveyId" (Long/parseLong (:id form-definition)))}
+                                              {:start-cursor cursor
+                                               :chunk-size page-size
+                                               :limit page-size}))))
 
 (defn question-type-map
   "Builds a map from question id to type"
@@ -119,11 +122,11 @@
 (defn fetch-answers [ds form-definition form-instances]
   (let [question-types (question-type-map form-definition)]
     (reduce (fn [form-instance-answers form-instance-batch]
-              (reduce (fn [fia answer]
+              (reduce (fn [fia ^Entity answer]
                         (let [form-instance-id (str (.getProperty answer "surveyInstanceId"))
                               question-id (.getProperty answer "questionID")
                               response-str (or (.getProperty answer "value")
-                                               (.getValue (.getProperty answer "valueText")))
+                                               (.getValue ^Text (.getProperty answer "valueText")))
                               iteration (or (.getProperty answer "iteration") 0)
                               response (when response-str
                                          (parse-response (get question-types question-id)
@@ -143,7 +146,7 @@
             {}
             (partition-all 30 form-instances))))
 
-(defn form-instance-entity->map [form-instance]
+(defn form-instance-entity->map [^Entity form-instance]
   {:id (-> form-instance .getKey .getId str)
    :form-id (str (.getProperty form-instance "surveyId"))
    :surveyal-time (.getProperty form-instance "surveyalTime")
