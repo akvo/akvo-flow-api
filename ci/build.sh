d10cfb287c293dea91ec923aab44b4ad50c100bc
Iván Perdomo <ivan@perdomo.me>
2017-04-20 13:33:53 +0200

    Issue/20 gcloud setup (#39)
    * [#20] Install gcloud sdk if not present
    
    * [#20] Smarter check
    
    - The travis cache creates the $HOME/google-cloud-sdk folder if
      not present, we check if the bin folder exists
    
    * [#20] Delete empty dir created by Travis CI cache
    
    * [#20] Fix call to install gcloud sdk
    
    * [#20] Make sure the local binaries are first on $PATH
    
    - We want the newly installed SDK to be found first on $PATH
    - Install kubectl
    
    * [#20] Update gcloud components as part of the build
    
    * [#20] Adds service account credentials
    
    * [#20] Authentication and kubectl configuration
    
    * [#20] Add missing configuration of compute zone
    
    * [#20] Initial k8s deployment definition (together with @jonase)
    
    * [#20] Move deployment phase to different bash script
    
    * [#20] Remove duplicated gcloud and kubectl setup
    
    * [#20] Pin versions to have a more reproducible builds
    
    * [#20] Read client-id and secret from file instead of environment
    
    * [#20] Adds expiry_claim to control TTL
    
    * [#20] Include proxy container to deployment (together with @jonase)
    
    * [#20] Deployment changes
    - Config map, service and ingress definition
    - NOTE: We hardcoded the upstream http service for now but the
      idea is to solve the `resolver` problem in nginx/openresty
    
    * [#20] Recreates deployment
    
    * [#20] Build containers and deployments only in `develop` branch
    
    - Small optimization to skip building the Flow data access jar if
      is already present in the maven cache

--- ci/build.sh
+++ ci/build.sh
@@ -5,13 +5,15 @@ set -eu
 BRANCH_NAME="${TRAVIS_BRANCH:=unknown}"
 LOCAL_TEST_DATA_PATH="target/stub-server-1.0-SNAPSHOT/WEB-INF/appengine-generated"
 
-# Flow data access classes
+# Flow data access classes, only if not present in $HOME/.m2
 
-cd flow
+DATA_ACCESS_VERSION=$(sed -n -e 's|.*org\.akvo\.flow/data-access "\(.*\)"]$|\1|p' api/project.clj)
 
-./build.sh
-
-cd ..
+if [[ ! -f "${HOME}/.m2/repository/org/akvo/flow/data-access/${DATA_ACCESS_VERSION}/data-access-${DATA_ACCESS_VERSION}.jar" ]]; then
+    cd flow
+    ./build.sh
+    cd ..
+fi
 
 # Lein
 
@@ -42,12 +44,18 @@ mvn appengine:devserver_stop
 
 cd ..
 
-# Build docker image if branch is `develop`
+# Build docker images if branch is `develop`
 
 if [[ "${BRANCH_NAME}" != "develop" ]]; then
     echo "Skipping docker build"
     exit 0
 fi
+
+cd nginx
+
+docker build -t "${PROXY_IMAGE_NAME:=akvo/flow-api-proxy}" .
+
+cd ..
 
 cd api
 
@@ -61,4 +69,4 @@ find "${HOME}/.m2" \
      \) \
      -exec cp -v {} target/uberjar/ \;
 
-docker build -t "${DOCKER_IMAGE_NAME:=akvo/flow-api-backend}" .
+docker build -t "${BACKEND_IMAGE_NAME:=akvo/flow-api-backend}" .
