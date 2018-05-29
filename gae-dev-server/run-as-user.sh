2b60b3a635340ca98be458c6d1ae1394777e0916
Iván Perdomo <ivan@perdomo.me>
2018-05-29 15:56:53 +0200

    [#123] Add `run-as-user.sh` script, rebuild gcloud image

---
+++ gae-dev-server/run-as-user.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -eu
+
+NEW_UID=$(stat -c '%u' /app/src)
+NEW_GID=$(stat -c '%g' /app/src)
+
+groupmod -g "$NEW_GID" -o akvo >/dev/null 2>&1
+usermod -u "$NEW_UID" -o akvo >/dev/null 2>&1
+
+exec gosu akvo:akvo "$@"
