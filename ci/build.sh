e60c6d6f06beaa25cd5d0014354ca377bc4457a6
Iván Perdomo <ivan@perdomo.me>
2018-05-30 06:06:07 +0200

    [#123] Fix call when building uberjar
    - Fix some 'shellcheck' warnings

--- ci/build.sh
+++ ci/build.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-BRANCH_NAME="${TRAVIS_BRANCH:=unknown}"
+
 LOCAL_TEST_DATA_PATH="gae-dev-server/target/stub-server-1.0-SNAPSHOT/WEB-INF/appengine-generated"
 
 mkdir -p "$HOME/.m2/repository"
@@ -22,25 +22,18 @@ docker-compose -p akvo-flow-api-ci -f docker-compose.yml -f docker-compose.ci.ym
 
 docker run \
        --rm \
-       --volume "$PWD/nginx/":"/conf" \
+       --volume "$PWD/nginx/:/conf" \
        --entrypoint /usr/local/openresty/bin/openresty \
        openresty/openresty:1.11.2.3-alpine-fat -t -c /conf/nginx.conf
+(
+    cd nginx
+    docker build -t "${PROXY_IMAGE_NAME:=akvo/flow-api-proxy}" .
+)
 
-# Build docker images if branch is `develop`
 
-#if [[ "${BRANCH_NAME}" != "develop" ]] && [[ "${BRANCH_NAME}" != "master" ]]; then
-#    echo "Skipping docker build"
-#    exit 0
-#fi
+docker-compose -p akvo-flow-api-ci -f docker-compose.yml -f docker-compose.ci.yml run --no-deps tests dev/run-as-user.sh lein uberjar
 
-cd nginx
-
-docker build -t "${PROXY_IMAGE_NAME:=akvo/flow-api-proxy}" .
-
-cd ..
-
-docker-compose -p akvo-flow-api-ci -f docker-compose.yml -f docker-compose.ci.yml run --no-deps tests dev/setup-dev-user-in-container.sh 'lein uberjar'
-
-cd api
-
-docker build -t "${BACKEND_IMAGE_NAME:=akvo/flow-api-backend}" .
+(
+    cd api
+    docker build -t "${BACKEND_IMAGE_NAME:=akvo/flow-api-backend}" .
+)
