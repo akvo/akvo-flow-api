561c0927b32b8fcace079844cde8a3c932f9022f
Iván Perdomo <ivan@perdomo.me>
2018-05-30 15:37:24 +0200

    [#123] Drop gosu in favor of chpst
    * `chpst` binary is available in all major distributions as part
      of the `runit` package (size < 120K)
    * `gosu` is available in debian but not in alpine
    * `su-exec` is available in alpine but not in debian

--- gae-dev-server/run-as-user.sh
+++ gae-dev-server/run-as-user.sh
@@ -8,4 +8,4 @@ NEW_GID=$(stat -c '%g' /app/src)
 groupmod -g "$NEW_GID" -o akvo >/dev/null 2>&1
 usermod -u "$NEW_UID" -o akvo >/dev/null 2>&1
 
-exec gosu akvo:akvo "$@"
+exec chpst -u akvo:akvo -U akvo:akvo env HOME=/home/akvo "$@"
