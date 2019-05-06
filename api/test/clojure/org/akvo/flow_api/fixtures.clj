85e8f85f795e7b10d27ea471b312b70134389843
Daniel Lebrero Berna <dlebrero@gmail.com>
2019-05-06 12:22:12 +0200

    [#161] Give GAE dev server more time to start
    When Travis is having network troubles, 1 min is not enough for the GAE dev server to download the DB and start up

--- api/test/clojure/org/akvo/flow_api/fixtures.clj
+++ api/test/clojure/org/akvo/flow_api/fixtures.clj
@@ -22,7 +22,7 @@
            (= status# ::error) (throw (RuntimeException. (str "Failed: " ~msg) return#)))))))
 
 (defn wait-for-server [url]
-  (try-for (str "Not 200 for " url) 60
+  (try-for (str "Not 200 for " url) 180
     (= 200 (:status (clj-http.client/get url)))))
 
 (defn check-servers-up []
