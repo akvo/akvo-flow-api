4002307f5367d18f5aabd62fe6776f8e4ea8dd2e
Daniel Lebrero Berna <dlebrero@gmail.com>
2020-03-12 10:48:36 +0100

    [#214] Add tests for existing error handling logic

--- api/src/clojure/org/akvo/flow_api/middleware/anomaly.clj
+++ api/src/clojure/org/akvo/flow_api/middleware/anomaly.clj
@@ -10,14 +10,17 @@
       (catch ExceptionInfo e
         (anomaly/handle e)))))
 
+(defn translate-exception [e]
+  (if (or (.contains (.getMessage e) "Over Quota")
+        (.contains (.getMessage e) "required more quota"))
+    (an/too-many-requests "This application is temporarily over its serving quota." {})
+    (if (.contains (.getMessage e) "Please try again in 30 seconds")
+      (an/bad-gateway "The server encountered an error and could not complete your request. Please try again in 30 seconds." {})
+      (throw e))))
+
 (defn wrap-log-errors [handler]
   (fn [request]
     (try
       (handler request)
       (catch Throwable e
-        (if (or (.contains (.getMessage e) "Over Quota")
-                (.contains (.getMessage e) "required more quota"))
-          (an/too-many-requests "This application is temporarily over its serving quota." {})
-          (if (.contains (.getMessage e) "Please try again in 30 seconds")
-            (an/bad-gateway "The server encountered an error and could not complete your request. Please try again in 30 seconds." {})
-            (throw e)))))))
+        (translate-exception e)))))
---
+++ api/test/clojure/org/akvo/flow_api/middleware/anomaly_test.clj
@@ -0,0 +1,22 @@
+(ns org.akvo.flow-api.middleware.anomaly-test
+  (:require [clojure.test :refer :all]
+            [org.akvo.flow-api.middleware.anomaly :as anomaly])
+  (:import (java.io IOException)))
+
+(defn ex? [exception-or-message]
+  (try
+    (anomaly/translate-exception (if (= (type exception-or-message) String)
+                                   (IOException. exception-or-message)
+                                   exception-or-message))
+    (is false "Should never reach here")
+    (catch Exception e
+      (if (ex-data e)
+        (:org.akvo.flow-api/anomaly (ex-data e))
+        e))))
+
+(deftest exception-translation
+  (let [exception (ArrayIndexOutOfBoundsException. "Hi I am not special")]
+    (is (= exception (ex? exception))))
+  (is (= (ex? "... Over Quota ...") :org.akvo.flow-api.anomaly/too-many-requests))
+  (is (= (ex? "... required more quota ...") :org.akvo.flow-api.anomaly/too-many-requests))
+  (is (= (ex? "... Please try again in 30 seconds ...") :org.akvo.flow-api.anomaly/bad-gateway)))
