35daebd6821386461da67c40c9bdbc8dec3f1fdd
Daniel Lebrero Berna <dlebrero@gmail.com>
2020-05-13 10:24:30 +0200

    [#221] Fix promote script Y/n logic

--- ci/promote-test-to-prod.sh
+++ ci/promote-test-to-prod.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 docker run --rm -e SLACK_CLI_TOKEN -v ~/.config:/home/akvo/.config -v "$(pwd)":/app \
-  -it akvo/akvo-devops:20200512.132847.78aa475 \
+  -it akvo/akvo-devops:20200513.082032.37c61a6 \
   promote-test-to-prod.sh flow-api akvo-flow-api-version akvo-flow-api
