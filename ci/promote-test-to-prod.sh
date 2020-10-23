97a0f9ed6f933b810bea9ad7d00a273fe7967bd9
Daniel Lebrero Berna <dlebrero@gmail.com>
2020-10-23 12:46:46 +0200

    [#238] Update deploy script to send Zulip notifications
    Fix #238

--- ci/promote-test-to-prod.sh
+++ ci/promote-test-to-prod.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker run --rm -e SLACK_CLI_TOKEN -v ~/.config:/home/akvo/.config -v "$(pwd)":/app \
-  -it akvo/akvo-devops:20200513.082032.37c61a6 \
-  promote-test-to-prod.sh flow-api akvo-flow-api-version akvo-flow-api
+docker run --rm -e ZULIP_CLI_TOKEN -v ~/.config:/home/akvo/.config -v "$(pwd)":/app \
+  -it akvo/akvo-devops:20201023.101935.7dacd92 \
+  promote-test-to-prod.sh flow-api akvo-flow-api-version akvo-flow-api zulip flumen-dev
