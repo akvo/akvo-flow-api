5a7c5d244050f7f29bb902a50f75da5f5ac95bde
Daniel Lebrero Berna <dlebrero@gmail.com>
2021-01-08 16:31:01 +0100

    [#241] Added log to find out flaky build cause

--- nginx-auth0/test-cache.sh
+++ nginx-auth0/test-cache.sh
@@ -11,6 +11,7 @@ wait4ports nginx=tcp://localhost:8082 upstream=tcp://localhost:3000
 
 
 log check cache miss
+./api.sh "http://localhost:8082/flow/ok"
 ./api.sh "http://localhost:8082/flow/ok" 2>&1 | grep 'X-Cache-Status: MISS'
 log check cache miss again
 ./api.sh "http://localhost:8082/flow/" 2>&1 | grep 'X-Cache-Status: MISS'
