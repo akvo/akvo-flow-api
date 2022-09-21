0e0f8f87049f8a7d3bda165e5202fed4558fe0d4
dedenbangkit <mail@dedenbangkit.com>
2022-09-21 19:31:59 +0700

    [#130] Add stacktrace to middleware

--- api/src/clojure/org/akvo/flow_api/middleware/anomaly.clj
+++ api/src/clojure/org/akvo/flow_api/middleware/anomaly.clj
@@ -1,7 +1,8 @@
 (ns org.akvo.flow-api.middleware.anomaly
   (:require [org.akvo.flow-api.endpoint.anomaly :as anomaly]
             [org.akvo.flow-api.anomaly :as an]
-            [clojure.tools.logging :as log])
+            [clojure.tools.logging :as log]
+            [clojure.stacktrace :as stacktrace])
   (:import [clojure.lang ExceptionInfo]))
 
 (defn wrap-anomaly [handler]
@@ -23,5 +24,6 @@
     (try
       (handler request)
       (catch Throwable e
+        (stacktrace/print-stack-trace e)
         (log/error e (str "Error:" (.getMessage e)))
         (translate-exception e)))))
