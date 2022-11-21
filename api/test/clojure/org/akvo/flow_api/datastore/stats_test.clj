06e707ca83bcb65109dfb83adca1fcc86614e63e
dedenbangkit <mail@dedenbangkit.com>
2022-11-21 16:26:18 +0700

    [#249] Remove redundant let

--- api/test/clojure/org/akvo/flow_api/datastore/stats_test.clj
+++ api/test/clojure/org/akvo/flow_api/datastore/stats_test.clj
@@ -98,20 +98,17 @@
 (deftest test-number-question
   (ds/with-remote-api (:remote-api fixtures/*system*) "akvoflowsandbox"
     (let [dss (DatastoreServiceFactory/getDatastoreService)
-          form-id "145492013"
           ;; After how many (full) strokes does water start flowing?
-          question-id "146622024"]
-      (let [results (stats/number-question-stats dss {:formId form-id
-                                                      :questionId question-id})
-            test-values (select-keys results [:max :min :sum :count])]
-        (is
-         (= {:max 365.0
-             :min 0.0
-             :sum 17373
-             :count 129} test-values))
-        (is
-         (= (float (/ (:sum test-values) (:count test-values)))
-            (float (:mean results))))))))
+          results (stats/number-question-stats dss {:formId "145492013"
+                                                    :questionId "146622024"})]
+      (is
+       (= {:max 365.0
+           :min 0.0
+           :sum 17373
+           :count 129} (select-keys results [:max :min :sum :count])))
+      (is
+       (= (float (/ (:sum results) (:count results)))
+          (float (:mean results)))))))
 
 (deftest test-filter-anwers
   (ds/with-remote-api (:remote-api fixtures/*system*) "akvoflowsandbox"
